7f3d371b325edbd391392915d91f95ef8559ddc5
robert-cummins <robert.k.cummins@gmail.com>
2020-03-02 11:36:11 +1300

    Set up components and react-router

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
+import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>
+, document.getElementById('root'));
 
 
 serviceWorker.unregister();
