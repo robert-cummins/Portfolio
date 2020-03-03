ef1f0345ccf7e44974b9788c394f0cf280bde187
robert-cummins <robert.k.cummins@gmail.com>
2020-03-04 12:57:33 +1300

    Create project card

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
-import {Tabs, Tab} from 'react-mdl'
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions, Button, CardMenu, IconButton, CardText } from 'react-mdl'
 
 class Projects extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             activeTab: 0
         }
     }
 
-    render(){
-        return(
+    toggleCategories = () => {
+        if (this.state.activeTab === 0) {
+            return (
+                <Card shadow={5} style={{minWidth: '450', margin: "auto"}}>
+                    <CardTitle style={{color: '#fff', height: "176px", background: "url(https://reactjs.org/logo-og.png) center/cover"}}>React Project #1</CardTitle>
+                    <CardText>
+                        This is my amazing React App. Please have a look!
+                    </CardText>
+                    <CardActions>
+                        <Button colored>GitHub</Button>
+                        <Button colored>Deployed</Button>
+                    </CardActions>
+                    <CardMenu style={{color: "#fff"}}>
+                        <IconButton name="share"></IconButton>
+                    </CardMenu>
+                </Card>
+            )
+        } else {
+            return (
+                <div><h1>This is C#</h1></div>
+            )
+        }
+    }
+
+
+    render() {
+        return (
             <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabID) => this.setState({activeTab: tabID})} ripple>
-                   <Tab>React</Tab> 
-                   <Tab>C#</Tab> 
+                <Tabs activeTab={this.state.activeTab} onChange={(tabID) => this.setState({ activeTab: tabID })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>C#</Tab>
                 </Tabs>
+
+                <section className="projects-grid">
+                    <Grid className="projects-grid">
+                        <Cell col={12}>
+                            <div className="content">
+                                {this.toggleCategories()}
+                            </div> 
+                        </Cell>
+                    </Grid>
+                    
+                </section>
             </div>
         )
     }
