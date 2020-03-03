e67ff06c945fa73bc843766820ad0279b43b1037
robert-cummins <robert.k.cummins@gmail.com>
2020-03-04 12:31:25 +1300

    Add tabs to projects page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import {Tabs, Tab} from 'react-mdl'
 
 class Projects extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            activeTab: 0
+        }
+    }
+
     render(){
         return(
-            <div><h1>Projects Page</h1></div>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabID) => this.setState({activeTab: tabID})} ripple>
+                   <Tab>React</Tab> 
+                   <Tab>C#</Tab> 
+                </Tabs>
+            </div>
         )
     }
 }
